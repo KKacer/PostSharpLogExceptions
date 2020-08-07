ad73bcca773f07e8ae3f1824f83ee3cab4db2dbc
kasra.kacer <kasra.kacer@outlook.com>
2020-08-07 07:21:58 -0400

    Add project files.

---
+++ Aspects/ReportAndSwallowExceptionAttribute.cs
@@ -0,0 +1,59 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Aspects.Advices;
+using PostSharp.Aspects.Dependencies;
+using PostSharp.Serialization;
+using Serilog;
+using System;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    [AspectTypeDependency(AspectDependencyAction.Order, AspectDependencyPosition.After,
+      typeof(AddContextOnExceptionAttribute))]
+    [PSerializable]
+    public sealed class ReportAndSwallowExceptionAttribute : OnExceptionAspect //,IInstanceScopedAspect
+    {
+        //[ImportMember("TheLogger", IsRequired = true)]
+        //public Property<ILogger> LoggerProperty;
+        //public ILogger TheLogger { get; set; }
+
+        //public ReportAndSwallowExceptionAttribute(ILogger logger)
+        //{
+        //    TheLogger = logger;
+        //}
+        public override void OnException(MethodExecutionArgs args)
+        {
+            //LoggerProperty.Get().Error("Oops!!!! Error Happened ....");
+            Log.Error("Oh a very bad error happened!");
+            //TheLogger.Error("Error happened ...");
+            // Write the default exception information.
+            Console.WriteLine("Exception information");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine(args.Exception.ToString());
+            Console.WriteLine("--------------------------------------------------------------");
+            var additionalContext = (StringBuilder)args.Exception.Data["Context"];
+
+            // Write the additional information that was gathered by AddContextOnExceptionAttribute.
+            if (additionalContext != null)
+            {
+                Console.WriteLine("Additional context information (call stack with parameter values)");
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.Write(additionalContext.ToString());
+                Console.WriteLine("--------------------------------------------------------------");
+            }
+
+            // Ignore the exception.
+            Console.WriteLine("*** Ignoring the exception ***");
+            args.FlowBehavior = FlowBehavior.Continue;
+        }
+
+        //object IInstanceScopedAspect.CreateInstance(AdviceArgs adviceArgs)
+        //{
+        //    return this.MemberwiseClone();
+        //}
+
+        //void IInstanceScopedAspect.RuntimeInitializeInstance()
+        //{
+        //}
+    }
+}
---
+++ PostSharpLogExceptions/Program.cs
@@ -0,0 +1,47 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using PostSharp.Patterns.Diagnostics.Backends.Serilog;
+using ExceptionHandling;
+using Serilog;
+using System;
+
+namespace ConsumerTest
+{
+    class Program
+    {
+
+
+        //public ILogger TheLogger { get; set; }
+        //[ReportAndSwallowException(Log.Logger)]
+
+        [ReportAndSwallowException]
+        [Log]
+        public static void TryExceptionMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Log]
+        public static void TryLogMethod()
+        {
+            Log.Information("Everything is alright!");
+        }
+        static void Main(string[] args)
+        {
+            //TheLogger = new LoggerConfiguration()
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.File(@"Logs\LogByAspect.log", rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+
+            //var theLogger = new LoggerConfiguration()
+            //        .WriteTo.File(@"Logs\LogByAspect.log", rollingInterval: RollingInterval.Day)
+            //        .CreateLogger();
+
+            LoggingServices.DefaultBackend = new SerilogLoggingBackend(Log.Logger); // or TheLogger
+
+            Console.WriteLine("Hello World!");
+            TryLogMethod();
+            TryExceptionMethod();
+
+        }
+    }
+}
